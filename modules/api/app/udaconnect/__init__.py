1a97500f7d6937b1fd5b1e2715ffc3fb1b1d241f
Lauren Ferreira <lauren.ferremch@gmail.com>
2022-02-10 18:52:02 -0300

    feat: reorganize folders

--- modules/api/app/udaconnect/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.udaconnect.models import Connection, Location, Person  # noqa
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
-
-
-def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_api
-
-    api.add_namespace(udaconnect_api, path=f"/{root}")
